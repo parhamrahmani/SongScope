9cd8a2e913a0ed0ee82df68959ca0611b2e58730
parham@PopOs <parhamrahmani13762@gmail.com>
2024-06-07 21:43:43 +0200

    updating initial_setup.py

--- initial_setup.py
+++ initial_setup.py
@@ -1,9 +1,19 @@
+import pymongo
 from pymongo import MongoClient
-from conf.mongodb.setup import setup_db, setup_collections, insert_example_tracks, insert_example_albums, insert_example_artists
-from conf.jsontools.tools import extract_and_transform_tracks
+from conf.mongodb.setup import (
+    setup_db,
+    setup_collections
+)
+from conf.mongodb.insertion import (insert_tracks,
+                                    insert_albums,
+                                    insert_artists,
+                                    insert_playlist)
+from conf.jsontools.tools import extract_and_transform_tracks, extract_and_transform_playlists, \
+    extract_and_transform_liked_songs
 from conf.jsontools.json_loader import load_data
+from conf.misc.parsing import extract_weights_from_url
 
-# Load your data (this function should be implemented to return the JSON data)
+# Load your data
 data = load_data('example_json_files/example_tracks.json')
 
 # Extract and transform the data
@@ -11,21 +21,34 @@ tracks_json, albums_json, artists_json = extract_and_transform_tracks(data)
 
 MONGODB_CLIENT = MongoClient("mongodb://localhost:27017/")
 DB_NAME = "spotifydb"
-COLLECTIONS = ["liked_songs", "albums", "artists"]
+COLLECTIONS = ["liked_songs", "albums", "artists", "playlists", "recommendations"]
 
 # Setup database and collections
 setup_db(MONGODB_CLIENT, DB_NAME)
 setup_collections(MONGODB_CLIENT, DB_NAME, COLLECTIONS)
+print(
+    "Initial Database setup completed. Created database 'spotifydb' and collections 'liked_songs', 'albums', 'artists', 'playlists', 'recommendations'.")
 
 # Insert example data
-insert_example_tracks(MONGODB_CLIENT, DB_NAME, tracks_json)
-insert_example_albums(MONGODB_CLIENT, DB_NAME, albums_json)
-insert_example_artists(MONGODB_CLIENT, DB_NAME, artists_json)
+print("Inserting example data... Track Data")
+insert_tracks(MONGODB_CLIENT, DB_NAME, tracks_json)
+insert_albums(MONGODB_CLIENT, DB_NAME, albums_json)
+insert_artists(MONGODB_CLIENT, DB_NAME, artists_json)
+print("First round of data insertion completed.")
 
-# Second round of data insertion
-data = load_data('example_json_files/example_track.json')
-track_json, album_json, artist_json = extract_and_transform_tracks(data)
+print("Second round of data insertion... Playlist data")
+data = load_data('example_json_files/example_playlist.json')
+playlist_json = extract_and_transform_playlists(data)
+insert_playlist(MONGODB_CLIENT, DB_NAME, playlist_json)
+print("Second round of data insertion completed.")
 
-insert_example_tracks(MONGODB_CLIENT, DB_NAME, track_json)
-insert_example_albums(MONGODB_CLIENT, DB_NAME, album_json)
-insert_example_artists(MONGODB_CLIENT, DB_NAME, artist_json)
+print("Third round of data insertion... Liked Songs data")
+data = load_data('example_json_files/example_liked_tracks.json')
+liked_songs_json = extract_and_transform_liked_songs(data)
+tracks_json, albums_json, artists_json = extract_and_transform_tracks(liked_songs_json)
+insert_tracks(MONGODB_CLIENT, DB_NAME, tracks_json)
+insert_albums(MONGODB_CLIENT, DB_NAME, albums_json)
+insert_artists(MONGODB_CLIENT, DB_NAME, artists_json)
+print("Third round of data insertion completed.")
+
+
--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+from flask import Flask, request, redirect, session
 from flask import redirect, session, render_template
 from conf import *
+import conf.spotifyapi.spotifyauth
+import conf.spotifyapi.spotfiyapi_functions
+import openai
 from pydantic import BaseModel
 
 
