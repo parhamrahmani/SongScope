65e9c0e6d8bd5de146516ec3d5d05184d2028e84
parham@PopOs <parhamrahmani13762@gmail.com>
2024-06-08 22:08:35 +0200

    mongodb funcs

--- conf/mongodb/query.py
+++ conf/mongodb/query.py
@@ -1,11 +1,5 @@
 from pymongo import MongoClient
 
-# Function to query all documents in a collection
-def collection_query_all(MONGODB_CLIENT, DB_NAME, COLLECTION):
-    db = MONGODB_CLIENT[DB_NAME]
-    results = list(db[COLLECTION].find())
-    return results
-
 # Function to query documents by specific fields
 def collection_query_by_fields(MONGODB_CLIENT, DB_NAME, COLLECTION, INPUT_DICT):
     db = MONGODB_CLIENT[DB_NAME]
@@ -14,16 +8,4 @@ def collection_query_by_fields(MONGODB_CLIENT, DB_NAME, COLLECTION, INPUT_DICT):
     doc = col.find(INPUT_DICT)
     return doc
 
-
-# Function to query multiple collections
-def query_mongodb(MONGODB_CLIENT, DB_NAME, COLLECTIONS):
-    db = MONGODB_CLIENT[DB_NAME]
-    results = {}
-
-    # Query the collections
-    for collection in COLLECTIONS:
-        print(f"Querying collection: {collection}")
-        results[collection] = list(db[collection].find())
-
-    return results
 
