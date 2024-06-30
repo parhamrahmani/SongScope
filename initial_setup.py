e64d145b91155cc9651b2da9d579ba7f2d2a056a
parham@PopOs <parhamrahmani13762@gmail.com>
2024-06-30 17:28:42 +0200

    The necessary script to prepare the application
    - has to be updated !

--- initial_setup.py
+++ initial_setup.py
@@ -1,3 +1,36 @@
+"""
+This will initialize the Project, there are also more prerequisites to be done before running this script.
+
+0. Read the Spotify API documentation: https://developer.spotify.com/documentation/web-api/
+to create an application there and get the client_id and client_secret and also set the addresses and callback URIs.
+Otherwise nothing will work.
+
+1. in your .env file, add the following variables:
+    - SPOTIFY_CLIENT_ID -> get from Spotify Developer Dashboard
+    - SPOTIFY_CLIENT_SECRET -> get from Spotify Developer Dashboard
+    - REDIRECT_URI -> where you call the callback function, e.g. http://localhost:5000/callback
+    - OPENAI_API_KEY -> get from OpenAI API Dashboard
+    - OPENAI_CLIENT -> use the OPENAI_API_KEY to create a client
+    - AI_MODEL -> the model you want to use, e.g. "gpt-3.5-turbo"
+    - OPEN_AI_ASSISTANT_ID -> get from OpenAI API Dashboard
+    - MONGODB_URI -> the URI to your MongoDB database, e.g. "mongodb://localhost:27017/"
+    - CHROMADB_PATH -> the path to your ChromaDB database, e.g. "data"
+    - FLASK_SECRET_KEY -> a secret key for Flask, e.g. "my_secret" you can set this as anything. remember to keep it
+    secret.
+    - TAVILY_KEY -> get from Tavily API Dashboard
+
+2. Make sure you have the required libraries installed:
+    - pymongo -> MongoDB Python driver
+    - openai -> OpenAI Python client
+    - chroma -> ChromaDB Python client
+    - flask -> Flask web framework
+    - requests -> HTTP requests library
+    - python-dotenv -> Python dotenv library to load environment variables
+    - chainlit -> Chainlit Python client
+
+
+"""
+
 import pymongo
 from pymongo import MongoClient
 from conf.mongodb.setup import (
@@ -10,11 +43,10 @@ from conf.mongodb.insertion import (insert_tracks,
                                     insert_playlist)
 from conf.jsontools.tools import extract_and_transform_tracks, extract_and_transform_playlists, \
     extract_and_transform_liked_songs
-from conf.jsontools.json_loader import load_data
-from conf.misc.parsing import extract_weights_from_url
+from conf.jsontools.tools import load_json
 
 # Load your data
-data = load_data('example_json_files/example_tracks.json')
+data = load_json('data/examples/example_tracks.json')
 
 # Extract and transform the data
 tracks_json, albums_json, artists_json = extract_and_transform_tracks(data)
@@ -37,13 +69,13 @@ insert_artists(MONGODB_CLIENT, DB_NAME, artists_json)
 print("First round of data insertion completed.")
 
 print("Second round of data insertion... Playlist data")
-data = load_data('example_json_files/example_playlist.json')
+data = load_json('data/examples/example_playlist.json')
 playlist_json = extract_and_transform_playlists(data)
 insert_playlist(MONGODB_CLIENT, DB_NAME, playlist_json)
 print("Second round of data insertion completed.")
 
 print("Third round of data insertion... Liked Songs data")
-data = load_data('example_json_files/example_liked_tracks.json')
+data = load_json('data/examples/example_liked_tracks.json')
 liked_songs_json = extract_and_transform_liked_songs(data)
 tracks_json, albums_json, artists_json = extract_and_transform_tracks(liked_songs_json)
 insert_tracks(MONGODB_CLIENT, DB_NAME, tracks_json)
@@ -51,6 +83,5 @@ insert_albums(MONGODB_CLIENT, DB_NAME, albums_json)
 insert_artists(MONGODB_CLIENT, DB_NAME, artists_json)
 print("Third round of data insertion completed.")
 
-# exmaple query
 
 
