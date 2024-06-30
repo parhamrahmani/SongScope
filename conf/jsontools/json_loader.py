89e0ccac876f57ae0aa5be852af7ad226b451c09
parham@PopOs <parhamrahmani13762@gmail.com>
2024-06-30 17:09:43 +0200

    refactor: deleting unused code

--- conf/jsontools/json_loader.py
+++
@@ -1,8 +0,0 @@
-import json
-
-
-def load_data(json_path):
-    # Load your data from a file or any other source
-    with open(json_path, 'r') as file:
-        data = json.load(file)
-    return data
--- docs/issue7.md
+++
@@ -1,5 +0,0 @@
-# Issue 7
-
-## Searching the pitchfrok from the internet
-
-## any links to any pitchfork databses
